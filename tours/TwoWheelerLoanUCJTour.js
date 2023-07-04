efe620c7bedf20806051a2371030dc8c680b1473
Yash Aryan <yasharyan307@outlook.com>
2023-07-04 07:27:56 +0530

    Update TwoWheelerLoanUCJTour.js

--- tours/TwoWheelerLoanUCJTour.js
+++ tours/TwoWheelerLoanUCJTour.js
@@ -1,1 +1,17 @@
-console.log("Two wheeler loan tour will load")
+console.log("Two wheeler loan tour load")
+let TwoWheelerLoanUCJTour = new Shepherd.Tour({
+    tourName: 'Get a Two Wheeler loan',
+    instanceCaller: 'TwoWheelerLoanUCJTour',
+    defaultStepOptions: {
+        cancelIcon: {
+            enabled: true
+        },
+        classes: 'z-50',
+        scrollTo: {
+            behavior: 'smooth',
+            block: 'center'
+        },
+        arrow: true,
+    },
+    useModalOverlay: false,
+});
