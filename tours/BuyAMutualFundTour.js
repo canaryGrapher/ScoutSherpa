9a35b128c2393e9520bd0fd4edd2228deb29742d
Yash Aryan <yasharyan307@outlook.com>
2023-07-21 02:00:45 -0700

    Update BuyAMutualFundTour.js

--- tours/BuyAMutualFundTour.js
+++ tours/BuyAMutualFundTour.js
@@ -1,3 +1,149 @@
+let BuyAMutualFundTour = new Shepherd.Tour({
+    tourName: 'How to Buy a Mutual Fund?',
+    instanceCaller: 'BuyAMutualFundTour',
+    defaultStepOptions: {
+        cancelIcon: {
+            enabled: true
+        },
+        classes: 'z-50',
+        scrollTo: {
+            behavior: 'smooth',
+            block: 'center'
+        },
+        arrow: true,
+    },
+    useModalOverlay: false,
+});
+
+// General Notification
+BuyAMutualFundTour.addStep({
+    id: 'BuyAMutualFundTour_1',
+    title: "1/7",
+    text: 'Welcome to your retail internet banking dashboard. From here, we will show you how to buy a mutual fund.',
+    buttons: [
+        {
+            text: 'Next',
+            action: BuyAMutualFundTour.next
+        },
+        {
+            text: 'Cancel',
+            action: BuyAMutualFundTour.cancel
+        }
+    ],
+    pageLink: "/vpv/li/personal-banking/dashboardPage"
+});
+
+// Select the 'Investment and Insurance' button on the menubar on dashboard
+BuyAMutualFundTour.addStep({
+    id: "BuyAMutualFundTour_2",
+    title: "2/7",
+    text: "Move your mouse over Investments & Insurance",
+    attachTo: {
+        element: "#topbar > div.light-orange > div > div:nth-child(5)",
+        on: "left"
+    },
+    advanceOn: {
+        selector: '#topbar > div.light-orange > div > div:nth-child(5)',
+        event: 'mouseover',
+    },
+    pageLink: "/vpv/li/personal-banking/dashboardPage"
+})
+
+// Click on Buy Mutual Funds option in the drop-down menu > redirection
+BuyAMutualFundTour.addStep({
+    id: "BuyAMutualFundTour_3",
+    title: "3/7",
+    text: "Click on Buy Mutual Funds",
+    attachTo: {
+        element: "#topbar > div.light-orange > div > div:nth-child(5) > div",
+        on: "left"
+    },
+    advanceOn: {
+        selector: '#topbar > div.light-orange > div > div:nth-child(5) > div',
+        event: 'click',
+    },
+    pageLink: "/vpv/li/personal-banking/dashboardPage"
+})
+
+//Click on view all button on the page where investment categories are displayed
+BuyAMutualFundTour.addStep({
+    id: 'BuyAMutualFundTour_4',
+    title: '4/7',
+    text: "Click 'View All'",
+    attachTo: {
+
+        element: "#ListingTable3 > main > section:nth-child(3) > div > div > div.comp-funds > div > button.btn.btn-default.js-show",
+        on: "top"
+    },
+    advanceOn: {
+        selector: '#ListingTable3 > main > section:nth-child(3) > div > div > div.comp-funds > div > button.btn.btn-default.js-show',
+        event: 'click',
+    },
+    pageLink: "/VPV/LI/InvestmentsandInsurance/InvestOnline/MutualFunds/HomeDetails"
+})
+
+// User clicks on any one category from the tiles displayed
+BuyAMutualFundTour.addStep({
+    id: 'BuyAMutualFundTour_5',
+    title: '5/7',
+    text: "<p>Let's Buy a Mutual Fund by Clicking on the Categories below</p><br /> <p style='text-align: right'>or click Next to search for funds manually</p>",
+    attachTo: {
+        element: "#ListingTable3 > main > section:nth-child(3) > div > div > div.head-wrap > p",
+        on: "top"
+    },
+    advanceOn: {
+        selector: '#ListingTable3 > main > section:nth-child(3) > div > div > div.head-wrap > p',
+        event: 'click',
+    },
+    pageLink: "/VPV/LI/InvestmentsandInsurance/InvestOnline/MutualFunds/HomeDetails"
+})
+
+// Click on "Invest Now" on any one of the suggested funds
+BuyAMutualFundTour.addStep({
+    id: 'BuyAMutualFundTour_6',
+    title: '6/7',
+    text: "Click <strong>Invest Now</strong> on the mutual fund of your choice.",
+    attachTo: {
+        element: "#MFfundslist > li:nth-child(1) > div",
+        on: "top"
+    },
+    advanceOn: {
+        selector: '#MFfundslist > li:nth-child(1) > div',
+        event: 'click',
+    },
+    pageLink: "/VPV/LI/InvestmentsandInsurance/InvestOnline/MutualFunds/WhatsNewFundsListing"
+})
+
+// user is suggested to select one type of transaction type
+BuyAMutualFundTour.addStep({
+    id: 'BuyAMutualFundTour_7',
+    title: '7/7',
+    text: "<p>Please select the required transaction type</p><ul><li><strong>SIP</strong> - Regular investment</li><li><strong>One time</strong> - Lumpsum investment</li><li><strong>FIP</strong> - Regular investment at a frequency of your choice</li></ul>",
+    attachTo: {
+        element: "#MFfundsSellist > li > div > div > div.bf-cont.js-hidden.js-hidden0 > div > div > ul > li.form-item.typ-2 > div",
+        on: "top"
+    },
+    advanceOn: {
+        selector: '#MFfundsSellist > li > div > div > div.bf-cont.js-hidden.js-hidden0 > div > div > ul > li.form-item.typ-2 > div',
+        event: 'click',
+    },
+    pageLink: "/VPV/LI/InvestmentsandInsurance/InvestOnline/MutualFunds/FundSelectedListing-Edelweiss Multi Asset Allocation Fund Reg-G"
+})
+
+// print current step index
+window.addEventListener('DOMContentLoaded', () => {
+    // check last tour step from local storage
+    let currentStepIndex = localStorage.getItem('currentStepIndex');
+    let currentTourIndex = localStorage.getItem('tourInstanceCaller');
+    if (currentTourIndex === 'BuyAMutualFundTour') {
+        if (currentStepIndex != '1') {
+            BuyAMutualFundTour.show(Number(currentStepIndex));
+        }
+    }
+}, false)
+
+
+
 var HowToBuyAFastTagTour = new Shepherd.Tour({
     tourName: "How to buy a ICICI FastTag",
     instanceCaller: "HowToBuyAFastTagTour",
@@ -120,6 +266,164 @@ HowToBuyAFastTagTour.addStep({
         event: 'click',
     },
     pageLink: "/VPV/LI/Exclusive Offerings/BuyFastag/Landingpage"
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM content loaded. Loading tour")
+    // check last tour step from local storage 
+    let currentStepIndex = localStorage.getItem('currentStepIndex');
+    let currentTourIndex = localStorage.getItem('tourInstanceCaller');
+    // if current tour is active, continue
+    if (currentTourIndex === 'HowToMakeICICIBankCreditCardPaymentTour') {
+        if (Number(currentStepIndex) >= 1) {
+            HowToMakeICICIBankCreditCardPaymentTour.show(Number(currentStepIndex));
+        }
+    }
+}, false) 
+
+var HowToMakeICICIBankCreditCardPaymentTour = new Shepherd.Tour({
+    tourName: "How to make ICICI bank credit card payment?",
+    instanceCaller: "HowToMakeICICIBankCreditCardPaymentTour",
+    defaultStepOptions: {
+        cancelIcon: {
+            enabled: true
+        },
+        classes: 'z-50',
+        scrollTo: {
+            behavior: 'smooth',
+            block: 'center'
+        },
+        arrow: true,
+    },
+    useModalOverlay: false,
+});
+
+// Select the 'Cards & Loans' button on the menubar on dashboard
+HowToMakeICICIBankCreditCardPaymentTour.addStep({
+    id: "HowToMakeICICIBankCreditCardPaymentTour_1",
+    title: "1/8",
+    text: "Move your mouse over 'Cards & Loans'",
+    attachTo: {
+        element: "#topbar > div.light-orange > div > div:nth-child(4)",
+        on: "bottom"
+    },
+    advanceOn: {
+        selector: '#topbar > div.light-orange > div > div:nth-child(4)',
+        event: 'mouseover',
+    },
+    pageLink: "/vpv/li/personal-banking/dashboardPage"
+})
+
+// Select the 'Credit Cards' sub-link under 'Cards & Loans'
+HowToMakeICICIBankCreditCardPaymentTour.addStep({
+    id: "HowToMakeICICIBankCreditCardPaymentTour_2",
+    title: "2/8",
+    text: "Click on 'Credit Cards'",
+    attachTo: {
+        element: "#topbar > div.light-orange > div > div:nth-child(4) > div > div > div:nth-child(1) > a:nth-child(2)",
+        on: "left"
+    },
+    advanceOn: {
+        selector: "#topbar > div.light-orange > div > div:nth-child(4) > div > div > div:nth-child(1) > a:nth-child(2)",
+        event: 'mouseover',
+    },
+    pageLink: "/vpv/li/personal-banking/dashboardPage"
+})
+
+// Click on 'Pay Bill' button to pay the bill
+HowToMakeICICIBankCreditCardPaymentTour.addStep({
+    id: "HowToMakeICICIBankCreditCardPaymentTour_3",
+    title: "3/8",
+    text: "Click on 'Pay Bill' to pay your Credit Card bills <br />Note: You can review your 'Total Due Amount' and 'Due Date' above",
+    attachTo: {
+        element: "#credit-1 > div.pl-4.pt-2 > a",
+        on: "bottom"
+    },
+    advanceOn: {
+        selector: "#credit-1 > div.pl-4.pt-2 > a",
+        event: 'click',
+    },
+    pageLink: "/vpv/li/personal-banking/myacc/creditcards"
+})
+
+// Click on 'Pay Bill' button to pay the bill
+HowToMakeICICIBankCreditCardPaymentTour.addStep({
+    id: "HowToMakeICICIBankCreditCardPaymentTour_4",
+    title: "4/8",
+    text: "Click on 'Pay Bill' to pay your Credit Card bills <br />Note: You can review your 'Total Due Amount' and 'Due Date' above",
+    attachTo: {
+        element: "#credit-1 > div.pl-4.pt-2 > a",
+        on: "bottom"
+    },
+    advanceOn: {
+        selector: "#credit-1 > div.pl-4.pt-2 > a",
+        event: 'click',
+    },
+    pageLink: "/vpv/li/personal-banking/myacc/creditcards"
+})
+
+// Select account to pay from
+HowToMakeICICIBankCreditCardPaymentTour.addStep({
+    id: "HowToMakeICICIBankCreditCardPaymentTour_5",
+    title: "5/8",
+    text: "Select the account from the drop-down from which you want to pay for your Credit Card.",
+    attachTo: {
+        element: "#DispFormWithTableContent\\.Rix0_mr\\.C1",
+        on: "bottom"
+    },
+    advanceOn: {
+        selector: "#DispFormWithTableContent\\.Rix0_mr\\.C1",
+        event: 'click',
+    },
+    pageLink: "/VPV/LI/PaymentsandTransfer/FundsTransfer/TransferFundstoMyPayeesnew"
+})
+
+// selecting amount to pay
+HowToMakeICICIBankCreditCardPaymentTour.addStep({
+    id: "HowToMakeICICIBankCreditCardPaymentTour_6",
+    title: "6/8",
+    text: "Select the amount to pay. You have the following options to pay: <ul><li>Total Amount due: The amount that is due to be paid against your credit card bill.</li><li>Min amount due: The minimum amount that you need to pay in order to prevent a default on your credit card</li><li>Other amount: If you wish to make a part payment to your credit card</li><li>Current outstanding amount: If you wish to pay the amount you have spent in the current billing cycle.</li></ul>",
+    attachTo: {
+        element: "#Amount",
+        on: "top"
+    },
+    advanceOn: {
+        selector: "#Amount",
+        event: 'click',
+    },
+    pageLink: "/VPV/LI/PaymentsandTransfer/FundsTransfer/TransferFundstoMyPayeesnew"
+})
+
+// select a mode of verification to pay amount
+HowToMakeICICIBankCreditCardPaymentTour.addStep({
+    id: "HowToMakeICICIBankCreditCardPaymentTour_7",
+    title: "7/8",
+    text: "Select the mode of verification for payment.",
+    attachTo: {
+        element: "#DispFormWithTableContent\\.RowsetOtp2",
+        on: "right"
+    },
+    advanceOn: {
+        selector: "#DispFormWithTableContent\\.RowsetOtp2",
+        event: 'click',
+    },
+    pageLink: "/VPV/LI/PaymentsandTransfer/FundsTransfer/TransferFundstoMyPayeesnew"
+})
+
+// select a mode of verification to pay amount
+HowToMakeICICIBankCreditCardPaymentTour.addStep({
+    id: "HowToMakeICICIBankCreditCardPaymentTour_8",
+    title: "8/8",
+    text: "Click on next to continue paying your credit card bill.",
+    attachTo: {
+        element: "#CONTINUE_TRANSACTION",
+        on: "bottom"
+    },
+    advanceOn: {
+        selector: "#CONTINUE_TRANSACTION",
+        event: 'click',
+    },
+    pageLink: "/VPV/LI/PaymentsandTransfer/FundsTransfer/TransferFundstoMyPayeesnew"
 })
 
 window.addEventListener('DOMContentLoaded', () => {
