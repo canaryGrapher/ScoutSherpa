5a17f6e657a5c58466bfa84a34fead5abd7f050b
canaryGrapher <yasharyan307@outlook.com>
2023-08-03 13:08:34 +0530

    Updated new tours

--- tours/BuyAMutualFundTour.js
+++ tours/BuyAMutualFundTour.js
@@ -18,28 +18,11 @@ let BuyAMutualFundTour = new Shepherd.Tour({
     useModalOverlay: false
 });
 
-// General Notification
-BuyAMutualFundTour.addStep({
-    id: 'BuyAMutualFundTour_1',
-    title: '1/7',
-    text: 'Welcome to your retail internet banking dashboard. From here, we will show you how to buy a mutual fund.',
-    buttons: [
-        {
-            text: 'Next',
-            action: BuyAMutualFundTour.next
-        },
-        {
-            text: 'Cancel',
-            action: BuyAMutualFundTour.cancel
-        }
-    ],
-    pageLink: '/vpv/li/personal-banking/dashboardPage'
-});
 
 // Select the 'Investment and Insurance' button on the menubar on dashboard
 BuyAMutualFundTour.addStep({
-    id: 'BuyAMutualFundTour_2',
-    title: '2/7',
+    id: 'BuyAMutualFundTour_1',
+    title: '1/7',
     text: 'Move your mouse over Investments & Insurance',
     attachTo: {
         element: '#topbar > div.light-orange > div > div:nth-child(5)',
@@ -54,15 +37,15 @@ BuyAMutualFundTour.addStep({
 
 // Click on Buy Mutual Funds option in the drop-down menu > redirection
 BuyAMutualFundTour.addStep({
-    id: 'BuyAMutualFundTour_3',
-    title: '3/7',
+    id: 'BuyAMutualFundTour_2',
+    title: '2/7',
     text: 'Click on Buy Mutual Funds',
     attachTo: {
-        element: '#topbar > div.light-orange > div > div:nth-child(5) > div',
+        element: "#topbar > div.light-orange > div > div:nth-child(5) > div > div > div:nth-child(1) > a:has([src='PR2/L001/consumer/theme/dashboardRevamp/topMenuImages/RINVIN/MFRV.svg'])",
         on: 'left'
     },
     advanceOn: {
-        selector: '#topbar > div.light-orange > div > div:nth-child(5) > div',
+        selector: "#topbar > div.light-orange > div > div:nth-child(5) > div > div > div:nth-child(1) > a:has([src='PR2/L001/consumer/theme/dashboardRevamp/topMenuImages/RINVIN/MFRV.svg'])",
         event: 'click'
     },
     pageLink: '/vpv/li/personal-banking/dashboardPage'
@@ -70,17 +53,15 @@ BuyAMutualFundTour.addStep({
 
 //Click on view all button on the page where investment categories are displayed
 BuyAMutualFundTour.addStep({
-    id: 'BuyAMutualFundTour_4',
-    title: '4/7',
-    text: "Click 'View All'",
+    id: 'BuyAMutualFundTour_3',
+    title: '3/7',
+    text: "These are the top categories of Mutual Funds. Click on <strong>View All</strong> to see more categories",
     attachTo: {
-        element:
-            '#ListingTable3 > main > section:nth-child(3) > div > div > div.comp-funds > div > button.btn.btn-default.js-show',
-        on: 'top'
+        element: "#ListingTable3 > main > section:nth-child(3) > div > div > div.comp-funds",
+        on: 'right'
     },
     advanceOn: {
-        selector:
-            '#ListingTable3 > main > section:nth-child(3) > div > div > div.comp-funds > div > button.btn.btn-default.js-show',
+        selector: "#ListingTable3 > main > section:nth-child(3) > div > div > div.comp-funds > div > button.btn.btn-default.js-show",
         event: 'click'
     },
     pageLink:
@@ -89,17 +70,17 @@ BuyAMutualFundTour.addStep({
 
 // User clicks on any one category from the tiles displayed
 BuyAMutualFundTour.addStep({
-    id: 'BuyAMutualFundTour_5',
-    title: '5/7',
-    text: "<p>Let's Buy a Mutual Fund by Clicking on the Categories below</p><br /> <p style='text-align: right'>or click Next to search for funds manually</p>",
+    id: 'BuyAMutualFundTour_4',
+    title: '4/7',
+    text: "Let's start buying one of the top-rated mutual funds. Click on <strong>Top Rated Funds</strong> to begin.",
     attachTo: {
         element:
-            '#ListingTable3 > main > section:nth-child(3) > div > div > div.head-wrap > p',
-        on: 'top'
+            `#ListingTable3 > main > section:nth-child(3) > div > div > div.comp-funds > ul > a:has([onclick="callFundListing('topRated')"]) > li`,
+        on: 'right'
     },
     advanceOn: {
         selector:
-            '#ListingTable3 > main > section:nth-child(3) > div > div > div.head-wrap > p',
+            `#ListingTable3 > main > section:nth-child(3) > div > div > div.comp-funds > ul > a:has([onclick="callFundListing('topRated')"]) > li`,
         event: 'click'
     },
     pageLink:
@@ -108,38 +89,56 @@ BuyAMutualFundTour.addStep({
 
 // Click on "Invest Now" on any one of the suggested funds
 BuyAMutualFundTour.addStep({
-    id: 'BuyAMutualFundTour_6',
-    title: '6/7',
-    text: 'Click <strong>Invest Now</strong> on the mutual fund of your choice.',
+    id: 'BuyAMutualFundTour_5',
+    title: '5/7',
+    text: "Let's start with Equity funds. Click on the <strong>Invest Now</strong> button on the highlighted fund to start investing.",
     attachTo: {
-        element: '#MFfundslist > li:nth-child(1) > div',
+        element: '#equity > div > ul > li:first-child',
         on: 'top'
     },
     advanceOn: {
-        selector: '#MFfundslist > li:nth-child(1) > div',
+        selector: "#equity > div > ul > li:nth-child(1) > div > div > div > div.bf-action-wrap > button.btn.btn-default.investNow0.flInvest0",
         event: 'click'
     },
     pageLink:
-        '/VPV/LI/InvestmentsandInsurance/InvestOnline/MutualFunds/WhatsNewFundsListing'
+        "/VPV/LI/InvestmentsandInsurance/InvestOnline/MutualFunds/TopRatedFundsListing"
 });
 
 // user is suggested to select one type of transaction type
 BuyAMutualFundTour.addStep({
-    id: 'BuyAMutualFundTour_7',
-    title: '7/7',
+    id: 'BuyAMutualFundTour_6',
+    title: '6/7',
     text: '<p>Please select the required transaction type</p><ul><li><strong>SIP</strong> - Regular investment</li><li><strong>One time</strong> - Lumpsum investment</li><li><strong>FIP</strong> - Regular investment at a frequency of your choice</li></ul>',
     attachTo: {
         element:
-            '#MFfundsSellist > li > div > div > div.bf-cont.js-hidden.js-hidden0 > div > div > ul > li.form-item.typ-2 > div',
+            "#equity > div > ul > li > div > div > div.bf-cont.js-hidden.jsHidden0 > div > div > ul > li.form-item.typ-2 > div",
         on: 'top'
     },
     advanceOn: {
         selector:
-            '#MFfundsSellist > li > div > div > div.bf-cont.js-hidden.js-hidden0 > div > div > ul > li.form-item.typ-2 > div',
+            "#equity > div > ul > li > div > div > div.bf-cont.js-hidden.jsHidden0 > div > div > ul > li.form-item.typ-2 > div",
         event: 'click'
     },
     pageLink:
-        '/VPV/LI/InvestmentsandInsurance/InvestOnline/MutualFunds/FundSelectedListing-Edelweiss Multi Asset Allocation Fund Reg-G'
+        "/VPV/LI/InvestmentsandInsurance/InvestOnline/MutualFunds/TopRatedSelectedListing-360 ONE Focused Equity Reg-G"
+});
+
+BuyAMutualFundTour.addStep({
+    id: 'BuyAMutualFundTour_7',
+    title: '7/7',
+    text: 'Fill in the additional information and you are good to go! You would have invested in a Mutual Fund.',
+    buttons: [
+        {
+            text: 'Finish',
+            action: HowToBuyAFastTagTour.complete
+        },
+        {
+            text: 'Back',
+            action: HowToBuyAFastTagTour.back
+        }
+    ],
+    pageLink:
+        "/VPV/LI/InvestmentsandInsurance/InvestOnline/MutualFunds/TopRatedSelectedListing-360 ONE Focused Equity Reg-G"
 });
 
 // eslint-disable-next-line no-undef
@@ -459,6 +458,199 @@ HowToMakeICICIBankCreditCardPaymentTour.addStep({
         event: 'click',
     },
     pageLink: "/VPV/LI/PaymentsandTransfer/FundsTransfer/TransferFundstoMyPayeesnew"
+})
+
+// eslint-disable-next-line no-undef
+var WhatsOnBankAccountPageGuideMe = new Shepherd.Tour({
+    tourName: "Guide Me: What's on Bank Account Page",
+    instanceCaller: "WhatsOnBankAccountPageGuideMe",
+    defaultStepOptions: {
+        cancelIcon: {
+            enabled: true
+        },
+        classes: 'z-50',
+        scrollTo: {
+            behavior: 'smooth',
+            block: 'center'
+        },
+        arrow: true,
+    },
+    useModalOverlay: true,
+});
+
+WhatsOnBankAccountPageGuideMe.addStep({
+    id: "WhatsOnBankAccountPageGuideMe_1",
+    title: "1/8",
+    text: "Welcome to your account page. Here you can view your account summary, account details, and account statements.",
+    buttons: [
+        {
+            text: 'Next',
+            action: HowToBuyAFastTagTour.next
+        },
+        {
+            text: 'Cancel',
+            action: HowToBuyAFastTagTour.cancel
+        }
+    ],
+    pageLink: "/vpv/li/personal-banking/myacc/bankacc"
+})
+
+WhatsOnBankAccountPageGuideMe.addStep({
+    id: "WhatsOnBankAccountPageGuideMe_2",
+    title: "2/8",
+    text: "This is your account summary. Here you can view your account balance.",
+    attachTo: {
+        element: "#AccountsTable_TotalBalance-MonthsTurnover > tbody",
+        on: "bottom"
+    },
+    buttons: [
+        {
+            text: 'Next',
+            action: HowToBuyAFastTagTour.next
+        },
+        {
+            text: 'Back',
+            action: HowToBuyAFastTagTour.back
+        }
+    ],
+    pageLink: "/vpv/li/personal-banking/myacc/bankacc"
+})
+
+WhatsOnBankAccountPageGuideMe.addStep({
+    id: "WhatsOnBankAccountPageGuideMe_3",
+    title: "3/8",
+    text: "List of all accounts with summary is displayed here. Select that account using the radio button to interact with it.",
+    attachTo: {
+        element: "#ListTableWithCtrlsDEH\\.Rowset1",
+        on: "bottom"
+    },
+    buttons: [
+        {
+            text: 'Next',
+            action: HowToBuyAFastTagTour.next
+        },
+        {
+            text: 'Back',
+            action: HowToBuyAFastTagTour.back
+        }
+    ],
+    pageLink: "/vpv/li/personal-banking/myacc/bankacc"
+})
+
+WhatsOnBankAccountPageGuideMe.addStep({
+    id: "WhatsOnBankAccountPageGuideMe_4",
+    title: "4/8",
+    text: "For the selected account, use the following buttons to interact with it. You can View the last 10 transactions, or get a detailed statement.",
+    attachTo: {
+        element: "#ListTableWithCtrlsDEH\\.Rb1_mr",
+        on: "bottom"
+    },
+    buttons: [
+        {
+            text: 'Next',
+            action: HowToBuyAFastTagTour.next
+        },
+        {
+            text: 'Back',
+            action: HowToBuyAFastTagTour.back
+        }
+    ],
+    pageLink: "/vpv/li/personal-banking/myacc/bankacc"
+})
+
+WhatsOnBankAccountPageGuideMe.addStep({
+    id: "WhatsOnBankAccountPageGuideMe_5",
+    title: "5/8",
+    text: "Click on this dropdown to view more options for the selected account.",
+    attachTo: {
+        element: "#ListTableWithCtrlsDEH\\.Rb1_mr\\.C16 > span.labelColumn_combo_small > span > div",
+        on: "left"
+    },
+    advanceOn: {
+        selector: "#ListTableWithCtrlsDEH\\.Rb1_mr\\.C16 > span.labelColumn_combo_small > span > div > div.selectedTxt",
+        event: 'click',
+    },
+    buttons: [
+        {
+            text: 'Next',
+            action: HowToBuyAFastTagTour.next
+        },
+        {
+            text: 'Back',
+            action: HowToBuyAFastTagTour.back
+        }
+    ],
+    pageLink: "/vpv/li/personal-banking/myacc/bankacc"
+})
+
+WhatsOnBankAccountPageGuideMe.addStep({
+    id: "WhatsOnBankAccountPageGuideMe_6",
+    title: "6/8",
+    text: "These are the list of service requests you can make for the selected account.",
+    attachTo: {
+        element: "#ListTableWithCtrlsDEH\\.Rb1_mr\\.C16 > span.labelColumn_combo_small > span > div > div.SSContainerDivWrapper",
+        on: "left"
+    },
+    buttons: [
+        {
+            text: 'Next',
+            action: HowToBuyAFastTagTour.next
+        },
+        {
+            text: 'Back',
+            action: HowToBuyAFastTagTour.back
+        }
+    ],
+    pageLink: "/vpv/li/personal-banking/myacc/bankacc"
+})
+
+WhatsOnBankAccountPageGuideMe.addStep({
+    id: "WhatsOnBankAccountPageGuideMe_7",
+    title: "7/8",
+    text: "Select the format for download of the file. You can select between PDF or XLX format.",
+    attachTo: {
+        element: "#NavPanel_mr\\.Rb1",
+        on: "bottom"
+    },
+    buttons: [
+        {
+            text: 'Next',
+            action: HowToBuyAFastTagTour.next
+        },
+        {
+            text: 'Back',
+            action: HowToBuyAFastTagTour.back
+        }
+    ],
+    pageLink: "/vpv/li/personal-banking/myacc/bankacc"
+})
+
+WhatsOnBankAccountPageGuideMe.addStep({
+    id: "WhatsOnBankAccountPageGuideMe_7",
+    title: "8/8",
+    text: "That is all for this tour. Hope you understood the options available on this page.",
+    buttons: [
+        {
+            text: 'Complete',
+            action: HowToBuyAFastTagTour.next
+        },
+        {
+            text: 'Back',
+            action: HowToBuyAFastTagTour.back
+        }
+    ],
+    pageLink: "/vpv/li/personal-banking/myacc/bankacc"
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM content loaded. Loading tour")
+    var button = document.createElement("Button");
+    button.innerHTML = "Guide Me";
+    button.onclick = function () {
+        WhatsOnBankAccountPageGuideMe.start();
+    };
+    button.style = "left:-25px;position:fixed;top:50%;z-index:9999;transform:rotate(90deg);color:white;background-color:rgb(239, 124, 0);border-radius: 5px 5px 0 0;padding:5px 13px;cursor:pointer;"
+    document.body.appendChild(button);
 })
 
 window.addEventListener('DOMContentLoaded', () => {
