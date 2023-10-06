37d530b44305b7fa34b2751d2fdaecf1f8f7aede
Yash Aryan <yasharyan307@outlook.com>
2023-10-06 13:49:11 +0530

    Added Overlay page for Bank Account page

--- tours/newRIBOverlayPages.js
+++ tours/newRIBOverlayPages.js
@@ -1,2 +1,34 @@
 // overlay page code will go here
 // Path: tours/newRIBOverlayPages.js
+var GuideMeWhatsOnNewRIBAccountsPage = new Shepherd.Tour({
+		tourName: "Guide Me: What's on new Bank Account Page",
+		instanaceCaller: "GuideMeWhatsOnNewRIBAccountsPage",
+		defaultStepOptions: {
+			cancelIcon: {
+				enabled: true
+			},
+			scrollTo: {
+				behavior: 'smooth',
+				block: 'center',
+			},
+			arrow: true
+		},
+		useModalOverlay: true
+});
+
+GuideMeWhatsOnNewRIBAccountsPage.addStep({
+		id: 'GuideMeWhatsOnNewRIBAccountsPage_1',
+		title: '1/4',
+		text: 'Welcome to your new Bank Accounts page. All your account information is visible here.',
+		buttons: [
+			{
+				text: 'Next',
+				action: GuideMeWhatsOnNewRIBAccountsPage.next
+			},
+			{
+				text: 'Cancel',
+				action: GuideMeWhatsOnNewRIBAccountsPage.cancel,
+				secondary: true
+			},
+			pageLink: '/bank-account'
+});
