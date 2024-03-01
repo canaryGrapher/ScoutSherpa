847767947f39b1129e47a4bf74a327930a706912
canaryGrapher <yasharyan307@outlook.com>
2024-03-02 01:37:57 +0530

    Changes

--- tours/newRIBDAPUtilities.js
+++ tours/newRIBDAPUtilities.js
@@ -1,4 +1,5 @@
-// Mar 1, 2024 | File updated
+// Mar 2, 2024 | File updated
+// update 34: Added page load logic
 // update 33: Added logic for button showing for edge cases
 // update 32: Fixed modal opening issue
 // update 31: Modal content change, invokation logic change and journey change
@@ -35,26 +36,26 @@
 let count = 0;
 let pageCount = 0;
 const journeyInfo = {
-    "/in/credit-card": {
-        journey: "document.querySelector('#dapModalCloseButton')?.click();NewRIBCreditCardPage()",
-        logic: (!window.find("EXPLORE MORE CREDIT CARDS") && document.querySelectorAll(".cardAnalysis")[0])
-    },
+  "/in/credit-card": {
+    journey: "document.querySelector('#dapModalCloseButton')?.click();NewRIBCreditCardPage()",
+    logic: (document.querySelectorAll(".cardAnalysis")[0]?.getBoundingClientRect().x > 0)
+  },
 };
 
 const addAndRemoveGlow = (buttonReference, modal) => {
-    buttonReference.classList.add('glow-indicator');
-    //   buttonReference.style.cursor = "not-allowed";
-    setTimeout(function () {
-        modal.remove()
-        buttonReference.classList.remove('glow-indicator');
-        // buttonReference.classList.remove('modalMinimized');
-        buttonReference.style.cursor = "pointer"
-    }, 2000); // 2000 milliseconds = 2 seconds
+  buttonReference.classList.add('glow-indicator');
+  //   buttonReference.style.cursor = "not-allowed";
+  setTimeout(function () {
+    modal.remove()
+    buttonReference.classList.remove('glow-indicator');
+    // buttonReference.classList.remove('modalMinimized');
+    buttonReference.style.cursor = "pointer"
+  }, 2000); // 2000 milliseconds = 2 seconds
 };
 
 const getModalText = (linkURL) => {
-    const currentPath = window.location.pathname;
-    const modalText = `<div id="modalContainer" class="modal">
+  const currentPath = window.location.pathname;
+  const modalText = `<div id="modalContainer" class="modal">
 <button class="close-button" id="dapModalCloseButton" type="btn">×</button>
 <h2>View Demo</h2>
 <p>
@@ -67,64 +68,64 @@ ${journeyInfo[currentPath] ? "Welcome to the new and improved ICICI Bank website
     ${journeyInfo[currentPath].logic ? '<button class="modalButtons" type="button" onclick=' + journeyInfo[currentPath].journey + '>Guide me</button>' : ''}
 </div>
 </div>`;
-    return modalText;
+  return modalText;
 }
 
 const showModal = (linkURL, topButtonSelector) => {
-    console.log("Invoking showModal()")
-    document.querySelector(topButtonSelector).style.cursor = "not-allowed"
-    const modalContent = getModalText(linkURL);
-    document.body.insertAdjacentHTML('beforeend', modalContent);
-    document.querySelector("#dapModalCloseButton").addEventListener('click', () => {
-        closeAction(document.querySelector('#modalContainer'), document.querySelector(topButtonSelector))
-    });
+  console.log("Invoking showModal()")
+  document.querySelector(topButtonSelector).style.cursor = "not-allowed"
+  const modalContent = getModalText(linkURL);
+  document.body.insertAdjacentHTML('beforeend', modalContent);
+  document.querySelector("#dapModalCloseButton").addEventListener('click', () => {
+    closeAction(document.querySelector('#modalContainer'), document.querySelector(topButtonSelector))
+  });
 }
 
 const closeAction = (modalReference, topButton) => {
-    console.log('Invoking closeAction()')
-    // const buttonReference = document.getElementById('closeButton')
-    calculateRetract(topButton, modalReference);
-    modalReference.setAttribute('class', 'modalMinimized')
-    addAndRemoveGlow(topButton, modalReference)
+  console.log('Invoking closeAction()')
+  // const buttonReference = document.getElementById('closeButton')
+  calculateRetract(topButton, modalReference);
+  modalReference.setAttribute('class', 'modalMinimized')
+  addAndRemoveGlow(topButton, modalReference)
 };
 
 const calculateRetract = (buttonReference, modalReference) => {
-    const elementDetails = buttonReference.getBoundingClientRect();
-    const modalDetails = modalReference.getBoundingClientRect();
-    var style = document.createElement('style');
-    style.innerHTML = `.modalMinimized {
+  const elementDetails = buttonReference.getBoundingClientRect();
+  const modalDetails = modalReference.getBoundingClientRect();
+  var style = document.createElement('style');
+  style.innerHTML = `.modalMinimized {
   -webkit - transform: translate(${elementDetails.left -
-        modalDetails.left -
-        (modalDetails.width - elementDetails.width) +
-        200
-        }px, -${modalDetails.top -
-        elementDetails.top -
-        (elementDetails.height - modalDetails.height) -
-        50
-        }px) scale(0);
+    modalDetails.left -
+    (modalDetails.width - elementDetails.width) +
+    200
+    }px, -${modalDetails.top -
+    elementDetails.top -
+    (elementDetails.height - modalDetails.height) -
+    50
+    }px) scale(0);
   transform: translate(${elementDetails.left -
-        modalDetails.left -
-        (modalDetails.width - elementDetails.width) +
-        200
-        }px, -${modalDetails.top -
-        elementDetails.top -
-        (elementDetails.height - modalDetails.height) -
-        50
-        }px) scale(0);
+    modalDetails.left -
+    (modalDetails.width - elementDetails.width) +
+    200
+    }px, -${modalDetails.top -
+    elementDetails.top -
+    (elementDetails.height - modalDetails.height) -
+    50
+    }px) scale(0);
 } `;
-    document.head.appendChild(style);
+  document.head.appendChild(style);
 };
 
 // eslint-disable-next-line no-unused-vars
 const associateModalForDAP = (linkURL, buttonSelector) => {
-    console.log("invoking associateModalForDAP()")
-    if (document.querySelector(buttonSelector)?.style.cursor != "not-allowed" && Array.from(document.querySelectorAll('.shepherd-element')).every(element => element.getBoundingClientRect().x === 0)
-    ) {
-        console.log("Conditions met for invoking modal")
-        if (count === 0) {
-            console.log('Creating new modal for the very first time');
-            const modalStyle = document.createElement('style');
-            modalStyle.innerHTML = `
+  console.log("invoking associateModalForDAP()")
+  if (document.querySelector(buttonSelector)?.style.cursor != "not-allowed" && Array.from(document.querySelectorAll('.shepherd-element')).every(element => element.getBoundingClientRect().x === 0)
+  ) {
+    console.log("Conditions met for invoking modal")
+    if (count === 0) {
+      console.log('Creating new modal for the very first time');
+      const modalStyle = document.createElement('style');
+      modalStyle.innerHTML = `
   .modal {
     position        : absolute;
     margin          : auto;
@@ -268,55 +269,60 @@ const associateModalForDAP = (linkURL, buttonSelector) => {
     }
   }
   `;
-            document.head.appendChild(modalStyle);
-            count++;
-        } else {
-            console.log("entered in show modal")
-            showModal(linkURL, buttonSelector);
-        }
+      document.head.appendChild(modalStyle);
+      count++;
+    } else {
+      console.log("entered in show modal")
+      showModal(linkURL, buttonSelector);
     }
+  }
 };
 
 // eslint-disable-next-line no-unused-vars
 const pageChangeInvokationDAP = () => {
-    // fnuction to handle opening of modal
-    const mainFunction = () => {
-        let ISODateToday = new Date()
-        let dateToday = ISODateToday.getDate()
-        let openTimes = window.localStorage.getItem("modalOpenTime")
-        let lastOpenDate = window.localStorage.getItem("modalOpenDateReference")
-        // if the local storage key-value is missing for openTimes, set it to 0
-        if (!openTimes) {
-            window.localStorage.setItem("modalOpenTime", 0)
-        }
-        // Modal will open automatically based on the defined condition
-        const openModalAutomatically = () => {
-            // if modal has been opened for less that 3 time automatically and date today is not equal to the last time it was opened
-            if (Number(openTimes) < 3 && (Number(lastOpenDate) != dateToday)) {
-                window.localStorage.setItem("modalOpenDateReference", dateToday)
-                window.localStorage.setItem("modalOpenTime", (Number(openTimes) + 1))
-                document.querySelector("#guided_Journey_Triggered")?.click()
-            }
-        }
-        openModalAutomatically()
+  // fnuction to handle opening of modal
+  const mainFunction = () => {
+    let ISODateToday = new Date()
+    let dateToday = ISODateToday.getDate()
+    let openTimes = window.localStorage.getItem("modalOpenTime")
+    let lastOpenDate = window.localStorage.getItem("modalOpenDateReference")
+    // if the local storage key-value is missing for openTimes, set it to 0
+    if (!openTimes) {
+      window.localStorage.setItem("modalOpenTime", 0)
     }
+    // Modal will open automatically based on the defined condition
+    const openModalAutomatically = () => {
+      // if modal has been opened for less that 3 time automatically and date today is not equal to the last time it was opened
+      if (Number(openTimes) < 3 && (Number(lastOpenDate) != dateToday)) {
+        window.localStorage.setItem("modalOpenDateReference", dateToday)
+        window.localStorage.setItem("modalOpenTime", (Number(openTimes) + 1))
+        document.querySelector("#guided_Journey_Triggered")?.click()
+      }
+    }
+    openModalAutomatically()
+  }
 
-    if (journeyInfo[window.location.pathname]) {
-        pageCount = 0;
-        console.log("FUNCTION CONDITION MET, wopning modal in 4 seconds.")
-        const mainFunctionLogic = () => {
-            if(journeyInfo[window.location.pathname].logic) {
-                mainFunction()
-            }
+  if (document.readyState === 'complete') {
+    if (journeyInfo[window.location.pathname] && document.querySelectorAll(".shepherd-element").length != 0) {
+      pageCount = 0;
+      console.log("FUNCTION CONDITION MET, wopning modal in 4 seconds.")
+      const mainFunctionLogic = () => {
+        if (journeyInfo[window.location.pathname].logic) {
+          mainFunction()
         }
-        setTimeout(mainFunctionLogic, 4000)
+      }
+      setTimeout(mainFunctionLogic, 4000)
     }
     else {
-        if (pageCount < 3) {
-            // increasing the number of retries.
-            pageCount = pageCount + 1;
-            // retry the function after 4 seconds
-            setTimeout(pageChangeInvokationDAP, 4000);
-        }
+      if (pageCount < 3) {
+        console.log("Trying for one more time out of three")
+        // increasing the number of retries.
+        pageCount = pageCount + 1;
+        // retry the function after 4 seconds
+        setTimeout(pageChangeInvokationDAP, 4000);
+      }
     }
+  } else {
+    setTimeout(pageChangeInvokationDAP, 2000);
+  }
 }
--- tours/newRIBOverlayPages.js
+++ tours/newRIBOverlayPages.js
@@ -1,213 +1,217 @@
 // Last update: March 1, 2024
-// Update 1: Final update as per department review
+// update 2: Added on
+// update 1: Final update as per department review
 
 // eslint-disable-next-line no-unused-vars
 var NewRIBCreditCardPage = () => {
-    // eslint-disable-next-line no-undef
-    var GuideMeWhatsOnNewRIBCreditCardPage = new Shepherd.Tour({
-        tourName: "Guide Me: What's on new Credit Card page",
-        instanceCaller: "GuideMeWhatsOnNewRIBCreditCardPage",
-        defaultStepOptions: {
-            cancelIcon: {
-                enabled: true,
-            },
-            scrollTo: {
-                behavior: "smooth",
-                block: "center",
-            },
-            arrow: true,
-        },
-        useModalOverlay: true,
-        exitOnEsc: true,
-        keyboardnavigation: true,
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.addStep({
-        id: "GuideMeWhatsOnNewRIBCreditCardPage_0",
-        title: "1/1",
-        text: "Looks like you don't have any credit card linked to your account. Come back to this journey when you link a credit card.",
-        showOn: () => {
-            // eslint-disable-next-line prettier/prettier
-            const toShowOrNotCheck1 = window.find("Get a feature packed ICICI Bank Card!");
-            const toShowOrNotCheck2 = window.find("A Card on the fly");
-            return toShowOrNotCheck1 && toShowOrNotCheck2;
-        },
-        buttons: [
-            {
-                text: "Understood",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.complete,
-            },
-        ],
-        canClickTarget: false,
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.addStep({
-        id: "GuideMeWhatsOnNewRIBCreditCardPage_1",
-        title: "1/8",
-        canClickTarget: false,
-        text: `<div>All your ICICI, co-brand, and other bank credit cards are visible on the tabs horizontally, and your connected cards can be viewed under the tabs that have badges with numbers.  <div style="text-align: center;"><img src="https://cdn.jsdelivr.net/gh/canaryGrapher/ScoutSherpa@newRIB/tours/images/connectedCards.gif" width="350" alt="Animation showing connected cards dropdown" style="object-fit: contain;" crossorigin="anonymous" /></div></div>
+  // eslint-disable-next-line no-undef
+  var GuideMeWhatsOnNewRIBCreditCardPage = new Shepherd.Tour({
+    tourName: "Guide Me: What's on new Credit Card page",
+    instanceCaller: "GuideMeWhatsOnNewRIBCreditCardPage",
+    defaultStepOptions: {
+      cancelIcon: {
+        enabled: true,
+      },
+      scrollTo: {
+        behavior: "smooth",
+        block: "center",
+      },
+      arrow: true,
+    },
+    useModalOverlay: true,
+    exitOnEsc: true,
+    keyboardnavigation: true,
+  });
+  GuideMeWhatsOnNewRIBCreditCardPage.addStep({
+    id: "GuideMeWhatsOnNewRIBCreditCardPage_0",
+    title: "1/1",
+    text: "Looks like you don't have any credit card linked to your account. Come back to this journey when you link a credit card.",
+    showOn: () => {
+      // eslint-disable-next-line prettier/prettier
+      const toShowOrNotCheck1 = window.find("Get a feature packed ICICI Bank Card!");
+      const toShowOrNotCheck2 = window.find("A Card on the fly");
+      return toShowOrNotCheck1 && toShowOrNotCheck2;
+    },
+    buttons: [
+      {
+        text: "Understood",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.complete,
+      },
+    ],
+    canClickTarget: false,
+  });
+  GuideMeWhatsOnNewRIBCreditCardPage.addStep({
+    id: "GuideMeWhatsOnNewRIBCreditCardPage_1",
+    title: "1/8",
+    canClickTarget: false,
+    text: `<div>All your ICICI, co-brand, and other bank credit cards are visible on the tabs horizontally, and your connected cards can be viewed under the tabs that have badges with numbers.  <div style="text-align: center;"><img src="https://cdn.jsdelivr.net/gh/canaryGrapher/ScoutSherpa@newRIB/tours/images/connectedCards.gif" width="350" alt="Animation showing connected cards dropdown" style="object-fit: contain;" crossorigin="anonymous" /></div></div>
         `,
-        attachTo: {
-            element: document.querySelector(".pill-container"),
-            on: "bottom",
-        },
-        buttons: [
-            {
-                text: "Next",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.next,
-            },
-            {
-                text: "Cancel",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.cancel,
-                secondary: true,
-            },
-        ],
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.addStep({
-        id: "GuideMeWhatsOnNewRIBCreditCardPage_2",
-        title: "2/8",
-        canClickTarget: false,
-        text: `<div>If you have more than three credit card accounts (including other bank credit cards), they are pushed into the dropdown at the right of this bar. Clicking on the card button will move it to the main bar and show the details.<div style="text-align: center;"><img src="https://cdn.jsdelivr.net/gh/canaryGrapher/ScoutSherpa@newRIB/tours/images/moreCards.gif" width="350" alt="Animation showing how to view more than three cards" style="object-fit: contain;" crossorigin="anonymous" /></div></div>`,
-        attachTo: {
-            element: document.querySelector(".pill-container"),
-            on: "bottom",
-        },
-        buttons: [
-            {
-                text: "Next",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.next,
-            },
-            {
-                text: "Back",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.back,
-                secondary: true,
-            },
-        ],
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.addStep({
-        id: "GuideMeWhatsOnNewRIBCreditCardPage_3",
-        title: "3/8",
-        text: "These are your card's basic information. You can see balances on your card, your card's CVV, expiry month and year, and even setup auto payment for your bill due amount. If you wish to pay your credit card bill right now, click on <strong>Pay Now</strong>.",
-        attachTo: {
-            element: document.querySelector(".ccLeftWrapper"),
-            on: "right",
-        },
-        buttons: [
-            {
-                text: "Next",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.next,
-            },
-            {
-                text: "Back",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.back,
-                secondary: true,
-            },
-        ],
-        canClickTarget: false,
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.addStep({
-        id: "GuideMeWhatsOnNewRIBCreditCardPage_4",
-        title: "4/8",
-        text: "This is your card's analysis view. View your spendings, limits, and available credit limit here.",
-        attachTo: {
-            element: document.querySelectorAll(".cardAnalysis")[0],
-            on: "left",
-        },
-        buttons: [
-            {
-                text: "Next",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.next,
-            },
-            {
-                text: "Back",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.back,
-                secondary: true,
-            },
-        ],
-        canClickTarget: false,
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.addStep({
-        id: "GuideMeWhatsOnNewRIBCreditCardPage_5",
-        title: "5/8",
-        canClickTarget: false,
-        text: "You can use <strong>MANAGE YOUR CREDIT CARD</strong> button to view various options for management of your card.",
-        attachTo: {
-            element: document.querySelector(".rewardPointInManageCards"),
-            on: "left",
-        },
-        buttons: [
-            {
-                text: "Next",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.next,
-            },
-            {
-                text: "Back",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.back,
-                secondary: true,
-            },
-        ],
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.addStep({
-        id: "GuideMeWhatsOnNewRIBCreditCardPage_6",
-        title: "6/8",
-        canClickTarget: false,
-        text: "Want to know where you spent using your card recently? You can view your current, past as well as EMI details from this section.",
-        attachTo: {
-            element: document.querySelector(".statementContainer"),
-            on: "right",
-        },
-        buttons: [
-            {
-                text: "Next",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.next,
-            },
-            {
-                text: "Back",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.back,
-                secondary: true,
-            },
-        ],
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.addStep({
-        id: "GuideMeWhatsOnNewRIBCreditCardPage_7",
-        title: "7/8",
-        canClickTarget: false,
-        text: "These are offers available exclusively for you.",
-        attachTo: {
-            element: document.querySelector(
-                // eslint-disable-next-line prettier/prettier
-                "body > app-root > div.main > div > div > div.page-content.login > div.container.g-0.dark-theme-border > div > app-credit-card-main > div > div.col-12.side-paddings.newLanding.ng-star-inserted > div.ccRightWrapper.ng-star-inserted > div > app-loan-cc > swiper-container"),
-            on: "right",
-        },
-        buttons: [
-            {
-                text: "Next",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.next,
-            },
-            {
-                text: "Back",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.back,
-                secondary: true,
-            },
-        ],
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.addStep({
-        id: "GuideMeWhatsOnNewRIBCreditCardPage_8",
-        title: "8/8",
-        canClickTarget: false,
-        when: {
-            complete: () => addAndRemoveGlow()
-        },
-        text: "That concludes the guide. Feel free to watch it again any time you are stuck.",
-        buttons: [
-            {
-                text: "Complete",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.complete,
-            },
-            {
-                text: "Back",
-                action: GuideMeWhatsOnNewRIBCreditCardPage.back,
-                secondary: true,
-            },
-        ],
-    });
-    GuideMeWhatsOnNewRIBCreditCardPage.start();
+    attachTo: {
+      element: document.querySelector(".pill-container"),
+      on: "bottom",
+    },
+    buttons: [
+      {
+        text: "Next",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.next,
+      },
+      {
+        text: "Cancel",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.cancel,
+        secondary: true,
+      },
+    ],
+  });
+  GuideMeWhatsOnNewRIBCreditCardPage.addStep({
+    id: "GuideMeWhatsOnNewRIBCreditCardPage_2",
+    title: "2/8",
+    canClickTarget: false,
+    text: `<div>If you have more than three credit card accounts (including other bank credit cards), they are pushed into the dropdown at the right of this bar. Clicking on the card button will move it to the main bar and show the details.<div style="text-align: center;"><img src="https://cdn.jsdelivr.net/gh/canaryGrapher/ScoutSherpa@newRIB/tours/images/moreCards.gif" width="350" alt="Animation showing how to view more than three cards" style="object-fit: contain;" crossorigin="anonymous" /></div></div>`,
+    attachTo: {
+      element: document.querySelector(".pill-container"),
+      on: "bottom",
+    },
+    buttons: [
+      {
+        text: "Next",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.next,
+      },
+      {
+        text: "Back",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.back,
+        secondary: true,
+      },
+    ],
+  });
+  GuideMeWhatsOnNewRIBCreditCardPage.addStep({
+    id: "GuideMeWhatsOnNewRIBCreditCardPage_3",
+    title: "3/8",
+    text: "These are your card's basic information. You can see balances on your card, your card's CVV, expiry month and year, and even setup auto payment for your bill due amount. If you wish to pay your credit card bill right now, click on <strong>Pay Now</strong>.",
+    attachTo: {
+      element: document.querySelector(".ccLeftWrapper"),
+      on: "right",
+    },
+    buttons: [
+      {
+        text: "Next",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.next,
+      },
+      {
+        text: "Back",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.back,
+        secondary: true,
+      },
+    ],
+    canClickTarget: false,
+  });
+  GuideMeWhatsOnNewRIBCreditCardPage.addStep({
+    id: "GuideMeWhatsOnNewRIBCreditCardPage_4",
+    title: "4/8",
+    text: "This is your card's analysis view. View your spendings, limits, and available credit limit here.",
+    attachTo: {
+      element: document.querySelectorAll(".cardAnalysis")[0],
+      on: "left",
+    },
+    buttons: [
+      {
+        text: "Next",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.next,
+      },
+      {
+        text: "Back",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.back,
+        secondary: true,
+      },
+    ],
+    canClickTarget: false,
+  });
+  GuideMeWhatsOnNewRIBCreditCardPage.addStep({
+    id: "GuideMeWhatsOnNewRIBCreditCardPage_5",
+    title: "5/8",
+    canClickTarget: false,
+    text: "You can use <strong>MANAGE YOUR CREDIT CARD</strong> button to view various options for management of your card.",
+    attachTo: {
+      element: document.querySelector(".rewardPointInManageCards"),
+      on: "left",
+    },
+    buttons: [
+      {
+        text: "Next",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.next,
+      },
+      {
+        text: "Back",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.back,
+        secondary: true,
+      },
+    ],
+  });
+  GuideMeWhatsOnNewRIBCreditCardPage.addStep({
+    id: "GuideMeWhatsOnNewRIBCreditCardPage_6",
+    title: "6/8",
+    canClickTarget: false,
+    text: "Want to know where you spent using your card recently? You can view your current, past as well as EMI details from this section.",
+    attachTo: {
+      element: document.querySelector(".statementContainer"),
+      on: "right",
+    },
+    buttons: [
+      {
+        text: "Next",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.next,
+      },
+      {
+        text: "Back",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.back,
+        secondary: true,
+      },
+    ],
+  });
+  GuideMeWhatsOnNewRIBCreditCardPage.addStep({
+    id: "GuideMeWhatsOnNewRIBCreditCardPage_7",
+    title: "7/8",
+    canClickTarget: false,
+    text: "These are offers available exclusively for you.",
+    attachTo: {
+      element: document.querySelector(
+        // eslint-disable-next-line prettier/prettier
+        "body > app-root > div.main > div > div > div.page-content.login > div.container.g-0.dark-theme-border > div > app-credit-card-main > div > div.col-12.side-paddings.newLanding.ng-star-inserted > div.ccRightWrapper.ng-star-inserted > div > app-loan-cc > swiper-container"),
+      on: "right",
+    },
+    buttons: [
+      {
+        text: "Next",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.next,
+      },
+      {
+        text: "Back",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.back,
+        secondary: true,
+      },
+    ],
+  });
+  GuideMeWhatsOnNewRIBCreditCardPage.addStep({
+    id: "GuideMeWhatsOnNewRIBCreditCardPage_8",
+    title: "8/8",
+    canClickTarget: false,
+    text: "That concludes the guide. Feel free to watch it again any time you are stuck.",
+    buttons: [
+      {
+        text: "Complete",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.complete,
+      },
+      {
+        text: "Back",
+        action: GuideMeWhatsOnNewRIBCreditCardPage.back,
+        secondary: true,
+      },
+    ],
+  });
+  ["complete", "cancel"].forEach((event) =>
+    GuideMeWhatsOnNewRIBCreditCardPage.on(event, () => {
+      // eslint-disable-next-line no-undef
+      addAndRemoveGlow();
+    }),
+  );
+  GuideMeWhatsOnNewRIBCreditCardPage.start();
 };
